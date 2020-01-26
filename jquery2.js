d88f142f28a6c39ee47c801e0a8f7d3f0285d3be
Nisho1001 <56435272+Nisho1001@users.noreply.github.com>
2020-01-26 01:51:34 +0100

    Create jquery2.js

---
+++ jquery2.js
@@ -0,0 +1,9 @@
+$( ".collapse" ).click(function() {
+  $( ".gallery" ).toggle( "slow", function() {
+  });
+});
+
+$('.collapse2').click(function() {
+  $(".banner").toggle("slow", function() {
+  });
+});
